497fc365698d41cba3969598522e7ed7a86c405e
JunYub Kim <kjyub08@naver.com>
2024-09-09 01:06:57 +0900

    페이지 별 동적 제목

--- src/app/game/[gameId]/page.tsx
+++ src/app/game/[gameId]/page.tsx
@@ -6,6 +6,8 @@ import VersusEditor from "@/components/versus/VersusEditor"
 import ApiUtils from "@/utils/ApiUtils"
 import AuthUtils from "@/utils/AuthUtils"
 import VersusGameView from "@/components/versus/VersusGameView"
+import Head from "next/head"
+import { Metadata } from "next"
 
 const getGame = async (gameId: string) => {
     const [bResult, statusCode, response] = await ApiUtils.request(
@@ -16,25 +18,29 @@ const getGame = async (gameId: string) => {
     return response
 }
 
+export async function generateMetadata({ params }: { gameId: string }): Promise<Metadata> {
+    let gameTitle: string = "VS 게임 추즈밍"
+
+    try {
+        const { gameId } = params
+    
+        const gameData = await getGame(gameId)
+        gameTitle = gameData["title"] + " - " + gameTitle
+    } catch {
+        //
+    }
+
+    return {
+        title: gameTitle,
+    }
+}
+
 export default async function GamePage({ params }: { gameId: string }) {
     const { gameId } = params
 
     const gameData = await getGame(gameId)
 
-    let gameTitle: string = ""
-
-    try {
-        gameTitle = gameData["title"]
-    } catch {
-        //
-    }
-
     return (
-        <>
-            {gameTitle !== "" && (
-                <title>{gameTitle} - VS 게임 추즈밍</title>
-            )}
-            <VersusGameView gameData={gameData} />
-        </>
+        <VersusGameView gameData={gameData} />
     )
 }
