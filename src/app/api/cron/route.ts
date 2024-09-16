37ad895e36124ec347547847be32f2f5e5361044
JunYub Kim <kjyub08@naver.com>
2024-09-16 22:14:48 +0900

    cron 요청 보안 처리

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,9 +1,14 @@
+import ApiUtils from "@/utils/ApiUtils"
 import GameUtils from "@/utils/GameUtils"
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 export const revalidate = 0
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+    if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+        return ApiUtils.notAuth()
+    }
+
     try {
         GameUtils.updateRelatedGames()
     } catch (error) {
