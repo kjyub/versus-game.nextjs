afe2a5fb48096c0c458325cd57ba117385a41a57
JunYub Kim <kjyub08@naver.com>
2024-10-27 02:19:45 +0900

    sitemap 동적 생성 추가

---
+++ src/app/api/versus/game/ids/route.ts
@@ -0,0 +1,32 @@
+import DBUtils from "@/utils/DBUtils"
+import { NextRequest, NextResponse } from "next/server"
+import bcryptjs from "bcryptjs"
+import ApiUtils from "@/utils/ApiUtils"
+import { NextApiRequest } from "next"
+import MVersusGame from "@/models/versus/MVersusGame"
+import { auth } from "@/auth"
+import VersusGameChoice from "@/types/versus/VersusGameChoice"
+import MFile from "@/models/file/MFile"
+import { nanoid } from "nanoid"
+import AuthUtils from "@/utils/AuthUtils"
+import { GameState, PrivacyTypes, ThumbnailImageTypes } from "@/types/VersusTypes"
+import CommonUtils from "@/utils/CommonUtils"
+import MUser from "@/models/user/MUser"
+import { UserRole } from "@/types/UserTypes"
+import mongoose, { mongo } from "mongoose"
+import MVersusGameView from "@/models/versus/MVersusGameView"
+import MVersusGameAnswer from "@/models/versus/MVersusGameAnswer"
+import { IPaginationResponse } from "@/types/common/Responses"
+import GameUtils from "@/utils/GameUtils"
+
+export async function GET(req: NextRequest) {
+    await DBUtils.connect()
+
+    const gameAll = await MVersusGame.find({ isDeleted: false })
+
+    const gameIds = gameAll.map((game) => String(game._id))
+
+    const result: IPaginationResponse = gameIds
+
+    return ApiUtils.response(result)
+}
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,35 @@
+import ApiUtils from '@/utils/ApiUtils'
 import { MetadataRoute } from 'next'
  
-export default function sitemap(): MetadataRoute.Sitemap {
-    return [
-        {
-            url: 'https://chooseming.com',
+// /product/sitemap/1.xml
+// export async function generateSitemaps() {
+//     // Fetch the total number of products and calculate the number of sitemaps needed
+//     return [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }]
+// }
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const [bResult, statusCode, response] = await ApiUtils.request(
+        `/api/versus/game/ids`,
+        "GET",
+    )
+
+    const gameIds: Array<string> = response as string[]
+
+    let siteMaps = [{
+        url: 'https://chooseming.com',
+        lastModified: new Date(),
+        changeFrequency: "weekly",
+        priority: 1,
+    }]
+
+    for (const gameId of gameIds) {
+        siteMaps.push({
+            url: `https://chooseming.com/game/${gameId}`,
             lastModified: new Date(),
-            changeFrequency: "weekly",
+            changeFrequency: "daily",
             priority: 1,
-        },
-    ]
+        })
+    }
+
+    return siteMaps
 }
