a5f939ea98c4ff236b9c4ebf77adf1f9d288b27d
JunYub Kim <kjyub08@naver.com>
2025-07-07 00:21:48 +0900

    refactor: 임베드 페이지 선택시 선택 기능 추가

--- src/components/versus/VersusGameEmbedView.tsx
+++ src/components/versus/VersusGameEmbedView.tsx
@@ -1,15 +1,24 @@
 'use client';
 import * as S from '@/styles/VersusStyles';
 import VersusGame from '@/types/versus/VersusGame';
+import VersusGameChoice from '@/types/versus/VersusGameChoice';
 import Link from 'next/link';
 import VersusGameHead from './VersusGameHead';
+import { cn } from '@/utils/StyleUtils';
+import { useMemo, useState } from 'react';
+import { ChoiceSelectStatus } from '@/types/VersusTypes';
 
 interface IVersusGameView {
   gameData: any | null;
 }
 export default function VersusGameEmbedView({ gameData = null }: IVersusGameView) {
-  const game = new VersusGame();
-  game.parseResponse(gameData);
+  const [selectedChoice, setSelectedChoice] = useState<VersusGameChoice>(new VersusGameChoice());
+
+  const game = useMemo(() => {
+    const game = new VersusGame();
+    game.parseResponse(gameData);
+    return game;
+  }, [gameData]);
 
   return (
     <S.GameViewLayout>
@@ -17,8 +26,12 @@ export default function VersusGameEmbedView({ gameData = null }: IVersusGameView
 
       <div className="grid grid-cols-2 gap-4 w-full p-4">
         {game.choices.map((choice) => (
-          <S.GameViewChoiceBox key={choice.id}>
-            <div className="check-icon">
+          <S.GameViewChoiceBox 
+            key={choice.id} 
+            onClick={() => setSelectedChoice(selectedChoice.id === choice.id ? new VersusGameChoice() : choice)} 
+            $status={selectedChoice.id ? (selectedChoice.id === choice.id ? ChoiceSelectStatus.SELECTED : ChoiceSelectStatus.UNSELECTED) : ChoiceSelectStatus.WAIT}
+          >
+            <div className={cn(['check-icon', { active: selectedChoice.id === choice.id }])}>
               <i className="fa-solid fa-circle-check"></i>
             </div>
             <span
@@ -37,7 +50,12 @@ export default function VersusGameEmbedView({ gameData = null }: IVersusGameView
         <Link
           href={`/game/${game.nanoId}`}
           target="_blank"
-          className="bg-indigo-600/70 hover:bg-indigo-700/70 text-white backdrop-blur-lg px-6 py-3 rounded-full text-lg font-medium flex items-center justify-center active:translate-y-2 transition-all duration-300"
+          className={cn([
+            'flex items-center justify-center px-6 py-3',
+            'rounded-full backdrop-blur-lg bg-indigo-600/70 hover:bg-indigo-700/70',
+            'text-white text-lg font-medium',
+            'mouse:hover:scale-105 active:translate-y-2 transition-all duration-300',
+          ])}
         >
           투표하러 가기
           <i className="fa-solid fa-arrow-up-right-from-square text-base ml-2"></i>
