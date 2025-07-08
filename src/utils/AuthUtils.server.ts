1c4d58301582a14c14ea7d9b1e2b453d23c08623
JunYub Kim <kjyub08@naver.com>
2025-07-09 01:07:22 +0900

    test: 에러 처리

--- src/utils/AuthUtils.server.ts
+++ src/utils/AuthUtils.server.ts
@@ -16,18 +16,22 @@ function isSessionAuth(session: Session | null) {
 export async function getUserOrGuestIdBySSR(session: Session | null) {
   let userId: string | mongoose.Types.ObjectId = '';
 
-  const cookieStore = await cookies();
+  try {
+    const cookieStore = await cookies();
 
-  // 유저 확인 없으면 게스트
-  if (isSessionAuth(session)) {
-    // @ts-ignore
-    userId = session?.user._id;
-  } else if (cookieStore.has(CookieConsts.GUEST_ID)) {
-    const guestIdCookie = cookieStore.get(CookieConsts.GUEST_ID);
-    userId = new mongoose.Types.ObjectId(guestIdCookie?.value ?? '');
-  } else {
-    const newGuestId = randomUUID() as string;
-    cookieStore.set(CookieConsts.GUEST_ID, newGuestId, { httpOnly: true });
+    // 유저 확인 없으면 게스트
+    if (isSessionAuth(session)) {
+      // @ts-ignore
+      userId = session?.user._id;
+    } else if (cookieStore.has(CookieConsts.GUEST_ID)) {
+      const guestIdCookie = cookieStore.get(CookieConsts.GUEST_ID);
+      userId = new mongoose.Types.ObjectId(guestIdCookie?.value ?? '');
+    } else {
+      const newGuestId = randomUUID() as string;
+      cookieStore.set(CookieConsts.GUEST_ID, newGuestId, { httpOnly: true });
+    }
+  } catch (error) {
+    console.error(error);
   }
 
   return userId;
