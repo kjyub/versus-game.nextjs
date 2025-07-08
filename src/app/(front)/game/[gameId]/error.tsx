4417bfb0332a1aed810505f84de44bc5674ab1ff
JunYub Kim <kjyub08@naver.com>
2025-07-08 23:13:12 +0900

    test: 에러 로그 추가

--- src/app/(front)/game/[gameId]/error.tsx
+++ src/app/(front)/game/[gameId]/error.tsx
@@ -1,8 +1,13 @@
 'use client';
 
 import CError from '@/components/commons/errors/Error';
+import { useEffect } from 'react';
 
-export default function ErrorPage() {
+export default function ErrorPage({ error }: { error: Error }) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
   return (
     <div>
       <CError message="게임 데이터를 불러오는데 실패했습니다." redirectUrl="/" />
